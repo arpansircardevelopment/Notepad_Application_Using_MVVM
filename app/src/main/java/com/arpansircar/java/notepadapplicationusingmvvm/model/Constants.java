3821c624b2694e79d3454f2ba4cbc15506be7a72
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-10-29 17:06:57 +0530

    Commented Constants.java class for better readability.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/model/Constants.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/model/Constants.java
@@ -1,11 +1,21 @@
 package com.arpansircar.java.notepadapplicationusingmvvm.model;
 
+/**
+ * The Constants class is tasked with saving information that will remain constants throughout the entirety of the program.
+ * With the help of the Constants class, all these values are grouped into a single place.
+ * In the event that we might need to change any of these values, we can do so easily ensuringthat any change is reflected everywhere in the application.
+ */
+
+/*The Constant class has been attributed as a final class.
+ *Using a the final keyword here ensures that the class cannot be extended.*/
 public final class Constants {
 
+    /*A final constructor ensures that this class cannot be instantiated.*/
     private Constants() {
     }
 
     /*Constants*/
+    /*These constants refer to any identifiers used, such as the name of the database, table, etc.*/
     public static final String TABLE_NAME = "notes_table";
     public static final String DATABASE_NAME = "notes.db";
     public static final String COLUMN_ID = "column_id";
@@ -14,6 +24,7 @@ public final class Constants {
     public static final String COLUMN_NAME_DATE = "column_date";
 
     /*Query Constants*/
+    /*The query constants are the queries that will be used for performing database transactions.*/
     public static final String QUERY_ALL_NOTES = "SELECT * FROM " + TABLE_NAME;
     public static final String QUERY_SINGLE_NOTE = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = :noteID";
     public static final String DELETE_ALL_NOTES = "DELETE FROM " + TABLE_NAME;
