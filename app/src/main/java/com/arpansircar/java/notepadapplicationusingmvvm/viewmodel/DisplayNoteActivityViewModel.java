2b66288e33e0c2f9af39fec5087bc1b15040522b
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-10-31 11:48:18 +0530

    Initialized DisplayNoteActivityViewModel.java class and added functionality to display and delete a single note.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/DisplayNoteActivityViewModel.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/DisplayNoteActivityViewModel.java
@@ -1,4 +1,25 @@
 package com.arpansircar.java.notepadapplicationusingmvvm.viewmodel;
 
-class DisplayNoteActivityViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.arpansircar.java.notepadapplicationusingmvvm.repository.NotesRepository;
+import com.arpansircar.java.notepadapplicationusingmvvm.room.NotesEntity;
+
+public class DisplayNoteActivityViewModel extends ViewModel {
+
+    private final NotesRepository notesRepository;
+
+    public DisplayNoteActivityViewModel() {
+        notesRepository = new NotesRepository();
+    }
+
+    public LiveData<NotesEntity> selectNoteMethod(int noteID) {
+        return notesRepository.selectNoteMethod(noteID);
+    }
+
+    public void deleteNoteMethod(NotesEntity notesEntity) {
+        notesRepository.deleteNoteMethod(notesEntity);
+    }
+
 }
