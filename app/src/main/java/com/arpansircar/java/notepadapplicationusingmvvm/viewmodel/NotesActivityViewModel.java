29c75d6adb5ad62844246d4b29a78fba31cec548
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-10-31 11:36:38 +0530

    Initialized the NotesActivityViewModel.java class to fetch all the notes present in the database.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/NotesActivityViewModel.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/NotesActivityViewModel.java
@@ -1,4 +1,23 @@
 package com.arpansircar.java.notepadapplicationusingmvvm.viewmodel;
 
-class NotesActivityViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.arpansircar.java.notepadapplicationusingmvvm.repository.NotesRepository;
+import com.arpansircar.java.notepadapplicationusingmvvm.room.NotesEntity;
+
+import java.util.List;
+
+public class NotesActivityViewModel extends ViewModel {
+
+    private final NotesRepository notesRepository;
+
+    public NotesActivityViewModel() {
+        notesRepository = new NotesRepository();
+    }
+
+    public LiveData<List<NotesEntity>> selectAllNotesMethod() {
+        return notesRepository.selectAllNotesMethod();
+    }
+
 }
