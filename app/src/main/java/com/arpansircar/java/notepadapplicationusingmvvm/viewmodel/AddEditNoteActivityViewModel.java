cb4bb0fc25c54df242b6690d28ceaadb438212ba
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-10-31 11:40:05 +0530

    Initialized the AddEditNoteActivityViewModel.java class to insert new notes and update existing notes in the database.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/AddEditNoteActivityViewModel.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/AddEditNoteActivityViewModel.java
@@ -1,4 +1,24 @@
 package com.arpansircar.java.notepadapplicationusingmvvm.viewmodel;
 
-class AddEditNoteActivityViewModel {
+import androidx.lifecycle.ViewModel;
+
+import com.arpansircar.java.notepadapplicationusingmvvm.repository.NotesRepository;
+import com.arpansircar.java.notepadapplicationusingmvvm.room.NotesEntity;
+
+public class AddEditNoteActivityViewModel extends ViewModel {
+
+    private final NotesRepository notesRepository;
+
+    public AddEditNoteActivityViewModel() {
+        notesRepository = new NotesRepository();
+    }
+
+    public void insertNoteMethod(NotesEntity notesEntity) {
+        notesRepository.insertNoteMethod(notesEntity);
+    }
+
+    public void updateNoteMethod(NotesEntity notesEntity) {
+        notesRepository.updateNoteMethod(notesEntity);
+    }
+
 }
