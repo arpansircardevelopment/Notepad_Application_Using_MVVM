2e34fabd59b644a295a01132e40a5da3a75fd130
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-10-31 13:11:40 +0530

    Commented DisplayNoteActivityViewModel.java for better readability.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/DisplayNoteActivityViewModel.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/viewmodel/DisplayNoteActivityViewModel.java
@@ -10,14 +10,18 @@ public class DisplayNoteActivityViewModel extends ViewModel {
 
     private final NotesRepository notesRepository;
 
+    /*A NotesRepository instance will be created every time the ViewModel class is instantiated.
+     * This instance will be used for performing all the database operations via the repository class.*/
     public DisplayNoteActivityViewModel() {
         notesRepository = new NotesRepository();
     }
 
+    /*The selectNoteMethod(...) will be called when the user selects a single note.*/
     public LiveData<NotesEntity> selectNoteMethod(int noteID) {
         return notesRepository.selectNoteMethod(noteID);
     }
 
+    /*The deleteNoteMethod(...) will be called when the user wants to delete the selected note.*/
     public void deleteNoteMethod(NotesEntity notesEntity) {
         notesRepository.deleteNoteMethod(notesEntity);
     }
