b9e2f6fea390f3838a5c1be8f2cdda46cc904a6a
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-11-02 11:09:40 +0530

    Commented NotesActivity.java for additional readability.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/view/NotesActivity.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/view/NotesActivity.java
@@ -19,34 +19,68 @@ import com.arpansircar.java.notepadapplicationusingmvvm.viewmodel.NotesActivityV
 
 import java.util.List;
 
+/**
+ * The onCreate method is the primary activity in the entire applications.
+ * This activity starts up and shows the user all the notes that have been created or the facility to add a new note using the floating action button.
+ * All the notes that have been created using this application and short details associated with them show up in the RecyclerView.
+ * The user can click on any of these notes to view the complete details of the note.
+ */
 public class NotesActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityNotesBinding activityNotesBinding;
     private NotesActivityViewModel notesActivityViewModel;
 
+    /*The onCreate method is the first method that is executed when the application starts up.
+     * Usually, in this method, such functions are executed that are to be performed only once.
+     * In this method, I've defined two other methods to be executed as soon as the application starts up and that are to be executed only once.
+     * The initializeDatabase() method is used to create an instance of the RoomDatabase.
+     * An application context is used because we want to use the same instance throughout the context of the application.
+     * The initializeViewModel() is used to create an instance of the ViewModel class associated with this activity.
+     * This ViewModel will be used to handle any configuration changes. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityNotesBinding = DataBindingUtil.setContentView(this, R.layout.activity_notes);
         initializeDatabase();
         initializeViewModel();
+    }
+
+    /*The onStart() is the next method executed after the onCreate() callback method.
+     * In this method, two methods are executed, namely the setOnClickListenerMethod() and the setObserverMethod().
+     * The setOnClickListenerMethod() method is tasked with setting the onClickListener for any and all buttons that might exist in the activity.
+     * Currently, the activity has a single floating action button so this button's onClickListener is set in this activity.
+     * The setObservableMethod() method is used to activate the Observer to observe changes in the LiveData object present in the ViewModel. */
+    @Override
+    protected void onStart() {
+        super.onStart();
         setOnClickListenerMethod();
         setObserverMethod();
     }
 
+    /*The initializeDatabase() method is used to create an instance of the NotesDatabase class.
+     * This single instance will be used for performing all the database transactions including the CRUD operations.
+     * For creating an instance of this class, the application context is used.
+     * I've assumed that using an application context will allow me to use the instance throughout the entirety of the application.*/
     private void initializeDatabase() {
         NotesDatabase.initializeDatabase(getApplicationContext());
     }
 
+    /*The initializeViewModel() method is used for initializing the NotesActivityViewModel instance with the ViewModel class.*/
     private void initializeViewModel() {
         notesActivityViewModel = new ViewModelProvider(this).get(NotesActivityViewModel.class);
     }
 
+    /*The setObserverMethod() method is used simply for activating the observer.
+     * This task is done at the Started state of the activity to allow it to start observing the changes in the LiveData as soon as the activity starts.
+     * If there are any changes in the database, i.e., if it returns a List of NotesEntity objects, the list is sent to the setRecyclerViewMethod().*/
     private void setObserverMethod() {
         final Observer<List<NotesEntity>> observer = this::setRecyclerViewMethod;
         notesActivityViewModel.selectAllNotesMethod().observe(this, observer);
     }
 
+    /*The setRecyclerViewMethod(...) is called when the observer observes a change in the LiveData.
+     * Upon calling this method, the List of NotesEntity objects are passed into this method which promptly passes it to the RecyclerViewAdapter class.
+     * Accordingly, the RecyclerView is populated with the required views and a new RecyclerView is displayed with the newly added notes.*/
     private void setRecyclerViewMethod(List<NotesEntity> notesEntityList) {
         RecyclerView recyclerView = activityNotesBinding.notesListRecyclerView;
         NotesAdapter notesAdapter = new NotesAdapter(notesEntityList);
@@ -55,10 +89,15 @@ public class NotesActivity extends AppCompatActivity implements View.OnClickList
         recyclerView.setAdapter(notesAdapter);
     }
 
+    /*The setOnClickListenerMethod() is used to set the onClickListener to the floating action button used in the activity.*/
     private void setOnClickListenerMethod() {
         activityNotesBinding.newNoteFloatingActionButton.setOnClickListener(this);
     }
 
+    /*The onClick(...) method allows us to intercept all the clicks placed within this method.
+     * Since, only a single view can be clicked in this activity, therefore, this method contains only a single if clause.
+     * The if clause checks if the floating action button has been clicked or not.
+     * If it has, an Intent object starts the AddEditActivity for the user to add a new note to the activity. */
     @Override
     public void onClick(View view) {
         if (view == activityNotesBinding.newNoteFloatingActionButton) {
