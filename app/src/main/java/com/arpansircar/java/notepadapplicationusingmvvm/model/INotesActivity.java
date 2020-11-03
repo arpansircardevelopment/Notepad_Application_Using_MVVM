931769c25142f70074cee3c11fd1f68816f8dafe
Arpan Sircar <59797469+arpansircardevelopment@users.noreply.github.com>
2020-11-03 13:09:56 +0530

    Added comments in INotesActivity.java file for added readability.

--- app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/model/INotesActivity.java
+++ app/src/main/java/com/arpansircar/java/notepadapplicationusingmvvm/model/INotesActivity.java
@@ -1,7 +1,13 @@
 package com.arpansircar.java.notepadapplicationusingmvvm.model;
 
-import com.arpansircar.java.notepadapplicationusingmvvm.room.NotesEntity;
-
+/**
+ * The INotesActivity interface is used for intercepting the clicks on the RecyclerView present in the NotesActivity.
+ * When the interface detects an event, it promptly transfers the required data to the overridden method.
+ */
 public interface INotesActivity {
+
+    /*The onNoteClicked(...) method is the overridden method.
+     * It is used to transfer the data from the RecyclerView to the onNoteClicked method in the NotesActivity.
+     * Using this data, the DisplayNoteActivity.java is triggered.*/
     void onNoteClicked(int noteID);
 }
